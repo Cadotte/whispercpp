7b107409aac89362d31fb5380815a22fe482c118
Antoine Cadotte <antoine.cadotte@gmail.com>
2024-12-18 15:08:25 -0500

    Update command.cpp (%Y%m%d_%H%M%S format)

--- examples/command/command.cpp
+++ examples/command/command.cpp
@@ -693,10 +693,10 @@ int process_general_transcription(struct whisper_context * ctx, audio_async & au
                 if(testState == true){
                     std::time_t t = std::time(nullptr);
                     char timestamp[100];
-                    std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&t));
+                    std::strftime(timestamp, sizeof(timestamp), "%Y%m%d_%H%M%S", std::localtime(&t));
                     std::string file_name = std::string(timestamp) + ".wav";
                     std::string logs_folder = get_logs_folder();
-                    std::string filepath = logs_folder + "/" + "2024-12-18-11:36:10.wav";
+                    std::string filepath = logs_folder + "/" + file_name;
                     wavWriter.open(filepath, WHISPER_SAMPLE_RATE, 16, 1);
                     wavWriter.write(pcmf32_cur.data(), pcmf32_cur.size());
                     wavWriter.close();
